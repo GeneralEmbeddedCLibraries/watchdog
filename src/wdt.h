95e074230f34e153f5d83427f76814ccaf618fb5
Žiga Miklošič <ziga.miklosic@seltron.si>
2024-07-02 10:33:33 +0200

    - replace tabs with spaces - updated template files

--- src/wdt.h
+++ src/wdt.h
@@ -31,34 +31,34 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 /**
- * 	Module version
+ *     Module version
  */
-#define WDT_VER_MAJOR			( 1 )
-#define WDT_VER_MINOR			( 2 )
-#define WDT_VER_DEVELOP			( 0 )
+#define WDT_VER_MAJOR           ( 1 )
+#define WDT_VER_MINOR           ( 2 )
+#define WDT_VER_DEVELOP         ( 0 )
 
 /**
- * 	Watchdog status
+ *     Watchdog status
  */
 typedef enum
 {
-	eWDT_OK				= 0x00U,		/**<Normal operation */
-	eWDT_ERROR			= 0x01U,		/**<General error */
-	eWDT_ERROR_INIT		= 0x02U,		/**<Initialization error  */
-	eWDT_ERROR_CFG		= 0x04U,		/**<Settings error */
+    eWDT_OK         = 0x00U,        /**<Normal operation */
+    eWDT_ERROR      = 0x01U,        /**<General error */
+    eWDT_ERROR_INIT = 0x02U,        /**<Initialization error  */
+    eWDT_ERROR_CFG  = 0x04U,        /**<Settings error */
 } wdt_status_t;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Functions
 ////////////////////////////////////////////////////////////////////////////////
-wdt_status_t 	wdt_init					(void);
-wdt_status_t 	wdt_is_init					(bool * const p_is_init);
-wdt_status_t 	wdt_hndl					(void);
-wdt_status_t 	wdt_start					(void);
-wdt_status_t 	wdt_task_report				(const wdt_task_opt_t task);
+wdt_status_t    wdt_init                    (void);
+wdt_status_t    wdt_is_init                 (bool * const p_is_init);
+wdt_status_t    wdt_hndl                    (void);
+wdt_status_t    wdt_start                   (void);
+wdt_status_t    wdt_task_report             (const wdt_task_opt_t task);
 wdt_status_t    wdt_task_set_enable         (const wdt_task_opt_t task, const bool enable);
 wdt_status_t    wdt_task_get_enable         (const wdt_task_opt_t task, bool * const p_enable);
-void			wdt_pre_reset_isr_callback	(void);		
+void            wdt_pre_reset_isr_callback  (void);
 
 #endif // __WDT_H
 
